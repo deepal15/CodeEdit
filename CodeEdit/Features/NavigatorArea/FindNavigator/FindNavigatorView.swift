0edf951f4fa775139f5d41d5bd62dfcec272c529
Austin Condiff <austin.condiff@gmail.com>
2023-11-02 09:21:25 -0500

    Added SearchTextField and started using it in the Navigator. Added functionality to Find Navigator UI.

--- CodeEdit/Features/NavigatorArea/FindNavigator/FindNavigatorView.swift
+++ CodeEdit/Features/NavigatorArea/FindNavigator/FindNavigatorView.swift
@@ -8,72 +8,24 @@
 import SwiftUI
 
 struct FindNavigatorView: View {
-
     @EnvironmentObject private var workspace: WorkspaceDocument
 
     private var state: WorkspaceDocument.SearchState {
         workspace.searchState ?? .init(workspace)
     }
 
-    @State private var searchText: String = ""
+    @State var currentFilter: String = ""
+    @State private var foundFilesCount: Int = 0
+    @State private var searchResultCount: Int = 0
 
     enum Filters: String {
         case ignoring = "Ignoring Case"
         case matching = "Matching Case"
     }
 
-    @State var currentFilter: String = ""
-
-    @State private var foundFilesCount: Int = 0
-
-    @State private var searchResultCount: Int = 0
-
     var body: some View {
         VStack {
-            VStack {
-                FindNavigatorModeSelector(state: state)
-                FindNavigatorSearchBar(state: state, title: "", text: $searchText)
-                HStack {
-                    Button {} label: {
-                        Text("In Workspace")
-                            .font(.system(size: 10))
-                    }.buttonStyle(.borderless)
-                    Spacer()
-                    Menu {
-                        Button {
-                            currentFilter = Filters.ignoring.rawValue
-                            state.ignoreCase = true
-                            state.search(searchText)
-                        } label: {
-                            Text(Filters.ignoring.rawValue)
-                        }
-                        Button {
-                            currentFilter = Filters.matching.rawValue
-                            state.ignoreCase = false
-                            state.search(searchText)
-                        } label: {
-                            Text(Filters.matching.rawValue)
-                        }
-                    } label: {
-                        HStack(spacing: 2) {
-                            Spacer()
-                            Text(currentFilter)
-                                .foregroundColor(currentFilter == Filters.matching.rawValue ?
-                                                 Color.accentColor : .primary)
-                                .font(.system(size: 10))
-                        }
-                    }
-                    .menuStyle(.borderlessButton)
-                    .frame(width: currentFilter == Filters.ignoring.rawValue ? 80 : 88)
-                    .onAppear {
-                        if currentFilter == "" {
-                            currentFilter = Filters.ignoring.rawValue
-                        }
-                    }
-                }
-            }
-            .padding(.horizontal, 10)
-            .padding(.vertical, 5)
+            FindNavigatorForm(state: state)
             Divider()
             HStack(alignment: .center) {
                 Text("\(self.searchResultCount) results in \(self.foundFilesCount) files")
@@ -81,9 +33,6 @@ struct FindNavigatorView: View {
             }
             Divider()
             FindNavigatorResultList()
-        }
-        .onSubmit {
-            state.search(searchText)
         }
         .onReceive(state.objectWillChange) { _ in
             self.searchResultCount = state.searchResultCount
